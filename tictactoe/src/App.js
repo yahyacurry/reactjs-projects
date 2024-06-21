cce98f9daebdb27ae628247ced8b6289f8f8e46f
yahyacurry <reduxwolverine@gmail.com>
2024-06-21 17:36:55 -0400

    tic tac toe

---
+++ tictactoe/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import TicTacToe from './Components/TicTacToe/TicTacToe';
+
+function App() {
+  return (
+    <di>
+     <TicTacToe/>
+    </di>
+  );
+}
+
+export default App;
